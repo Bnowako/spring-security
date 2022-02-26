0a19a1029ad484a71506fdd13209c18449016884
blaze98 <bnowakowski98@gmail.com>
2022-02-26 18:53:05 +0100

    add sample user service and controller

---
+++ ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/user/UserController.java
@@ -0,0 +1,20 @@
+package blaze98.ssiach2ex1.user;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public User getUser(@RequestParam Long id) {
+        return userService.getUserById(id);
+    }
+}
