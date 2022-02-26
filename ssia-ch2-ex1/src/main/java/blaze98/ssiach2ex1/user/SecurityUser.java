e449fbc905efe5c3cbb0cbd676553d9dbf8053b1
blaze98 <bnowakowski98@gmail.com>
2022-02-26 18:56:08 +0100

    [CH3] delete dummy user, and add security user that is extension of entity user. Separation of SecurityUser and User is better for readability.

--- ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/user/SecurityUser.java
+++ ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/user/SecurityUser.java
@@ -6,20 +6,27 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class DummyUser implements UserDetails {
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "READ");
+        return List.of(user::getAuthority);
     }
 
     @Override
     public String getPassword() {
-        return "123456";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "bill";
+        return user.getUsername();
     }
 
     @Override
