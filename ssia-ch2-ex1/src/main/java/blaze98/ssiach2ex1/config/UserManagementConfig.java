638df8f86b37024b2bf53b706af166557e287f7d
blaze98 <bnowakowski98@gmail.com>
2022-02-26 13:11:00 +0100

    separate configurations of web authorization and user management to different classes to improve structure

--- ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/config/UserManagementConfig.java
+++ ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/config/UserManagementConfig.java
@@ -2,8 +2,6 @@ package blaze98.ssiach2ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
+public class UserManagementConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -29,13 +27,4 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll();
-    }
-
 }
