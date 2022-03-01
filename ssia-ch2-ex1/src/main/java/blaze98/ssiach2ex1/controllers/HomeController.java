cb29fbeea32ee41963179df893c45746cad94b9f
blaze98 <bnowakowski98@gmail.com>
2022-03-01 08:27:38 +0100

    [CH5] change authentication to be injected

--- ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/controllers/HomeController.java
+++ ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/controllers/HomeController.java
@@ -1,8 +1,6 @@
 package blaze98.ssiach2ex1.controllers;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/hello")
-    public String hello() {
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        Authentication a = securityContext.getAuthentication();
-
+    public String hello(Authentication a) {
         return "Hello! " + a.getName();
     }
 }
