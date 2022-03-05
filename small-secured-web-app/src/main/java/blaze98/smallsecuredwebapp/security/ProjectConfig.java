d8c89a1dd5074c6fdf0956a3cd6a7b8053162ea6
blaze98 <bnowakowski98@gmail.com>
2022-03-05 11:05:28 +0100

    move password encoders bean declaration to separate file to break circular dependency issue

--- small-secured-web-app/src/main/java/blaze98/smallsecuredwebapp/security/ProjectConfig.java
+++ small-secured-web-app/src/main/java/blaze98/smallsecuredwebapp/security/ProjectConfig.java
@@ -1,29 +1,16 @@
 package blaze98.smallsecuredwebapp.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     private final AuthenticationProviderService authenticationProviderService;
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SCryptPasswordEncoder sCryptPasswordEncoder() {
-        return new SCryptPasswordEncoder();
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
