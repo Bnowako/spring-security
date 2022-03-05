d0b395793755bee654aec469dde975fc02843a1b
blaze98 <bnowakowski98@gmail.com>
2022-03-05 09:40:05 +0100

    [CH6] add small secured web app based on hands on chapter

---
+++ small-secured-web-app/src/main/java/blaze98/smallsecuredwebapp/SmallSecuredWebAppApplication.java
@@ -0,0 +1,13 @@
+package blaze98.smallsecuredwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSecuredWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallSecuredWebAppApplication.class, args);
+    }
+
+}
