5b2c67f17f8a9694438cbc241f1ed59f704ff842
blaze98 <bnowakowski98@gmail.com>
2022-03-05 11:11:18 +0100

    delete module that i was creating while reading the book, i will leave in this repo only hands-on chapters implementations

--- ssia-ch2-ex1/src/main/java/blaze98/ssiach2ex1/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package blaze98.ssiach2ex1.user;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
