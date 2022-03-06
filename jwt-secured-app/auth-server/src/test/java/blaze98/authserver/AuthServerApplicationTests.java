10700761ed18d5e4cd01ec31fc6c8271cc0cb1b4
blaze98 <bnowakowski98@gmail.com>
2022-03-06 14:36:25 +0100

    [CH11] add auth server spring template

---
+++ jwt-secured-app/auth-server/src/test/java/blaze98/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package blaze98.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
